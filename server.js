9d357af21bbb94e3e493a9ae3a8998f2c2cc206d
Man Phan <phandinhman@gmail.com>
2020-03-20 21:14:37 +0700

    Middleware

--- server.js
+++ server.js
@@ -5,7 +5,15 @@ dotEnv.config();
 
 const app = express();
 
-app.get('/', (req, res, next) => {
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+const middleware = (req, res, next) => {
+  console.log('My middleware');
+  next();
+}
+
+app.get('/', middleware, (req, res, next) => {
   res.send('Hello');
 });
 
@@ -14,3 +22,17 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is listening on ${PORT}`);
 });
+
+app.use(function (err, req, res, next) {
+  console.log('Middleware');
+});
+
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send({
+    status: 500,
+    message: err.message,
+    body: {},
+  });
+})
