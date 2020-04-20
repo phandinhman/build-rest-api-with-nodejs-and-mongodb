c29d864b152029721a28c996e381a9c8e32a40f9
Man Phan <phandinhman@gmail.com>
2020-04-21 01:38:24 +0700

    Middleware continue

--- middleware/joiSchemaValidation.js
+++ middleware/joiSchemaValidation.js
@@ -1,12 +1,35 @@
 const Joi = require('joi');
 
 const validateObjectSchema = (data, schema) => {
-  const result = Joi.validate(data, schema);
-  console.log('Joi Schema is running', result);
+  const result = Joi.validate(data, schema,{ convert: false });
+
+  if (result.error) {
+    const error = result.error.details.map(value => {
+      return {
+        error: value.message,
+        path: value.path,
+      }
+    });
+
+    return error;
+    
+  }
+
+  return null;
 };
 
 module.exports.validateBody = (schema) => {
   return (req, res, next) => {
-    validateObjectSchema(req.body, schema);
+    const error = validateObjectSchema(req.body, schema);
+    let response = {};
+
+    if (error) {
+        response.body = error;
+        response.message = 'Invalid fields';
+
+        return res.status(401).send(response);
+    }
+
+    return next();
   };
 }
